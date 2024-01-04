118dfd073b16ee9e68980972f8c43f266daa09d8
Stan Fakudze <u19184914@tuks.co.za>
2024-01-04 21:43:37 +0200

    1. Added FED interest rate data

--- Analysis_repo/sourceCode.py
+++ Analysis_repo/sourceCode.py
@@ -4,15 +4,20 @@ from Analysis_repo import notebook_import_library
 
 # datafile URL
 consumer_credit_data = notebook_import_library.pd.read_csv("Analysis_repo/Datasets/FRB_G13to23.csv", sep=",")
+interest_rate_data = notebook_import_library.pd.read_csv("Analysis_repo/Datasets/FRB_InterestRate13to23.csv", sep=",")
 
 # exception to catch what happens when dataset is called 
 class datasetNotFoundException(Exception):
     pass 
 
-# imports 
+# send data to notebook 
 def sendConsumerCreditData():
     credit_analysis_data = consumer_credit_data
     return credit_analysis_data
+
+def sendInterestRateData():
+    int_rate_data = interest_rate_data
+    return int_rate_data
 
 
 
