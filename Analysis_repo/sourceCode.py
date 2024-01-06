8d2458100e1317b0f6b084d2ea9362aba1f2bd0e
Stan Fakudze <u19184914@tuks.co.za>
2024-01-06 18:55:29 +0200

    1. Updated source code 2. Added plots on consumer credit 3. Formed desired Analysis schema for my analysis

--- Analysis_repo/sourceCode.py
+++ Analysis_repo/sourceCode.py
@@ -2,6 +2,11 @@
 import Analysis_repo
 from Analysis_repo import notebook_import_library
 
+# API Links
+
+
+# 
+
 # datafile URL
 consumer_credit_data = notebook_import_library.pd.read_csv("Analysis_repo/Datasets/FRB_G13to23.csv", sep=",")
 interest_rate_data = notebook_import_library.pd.read_csv("Analysis_repo/Datasets/FRB_InterestRate13to23.csv", sep=",")
@@ -10,21 +15,17 @@ interest_rate_data = notebook_import_library.pd.read_csv("Analysis_repo/Datasets
 class datasetNotFoundException(Exception):
     pass 
 
-# send data to notebook 
+# send Consumer credit data 
 def sendConsumerCreditData():
     credit_analysis_data = consumer_credit_data
     return credit_analysis_data
 
+# send Interest rate data
 def sendInterestRateData():
     int_rate_data = interest_rate_data
     return int_rate_data
 
+
 # Code upgrading ideas
 # connect data source via API COnnection instead of downloading dataset.
 
-
-# Datset saving code
-# # Save the DataFrame to a CSV file
-#df.to_csv('dataset.csv', index=False)
-
-# df.to_csv('Analysis_repo/Datasets/creditCardData_p2of2.csv', index=False)
